9e3a28a4981f7ea69c4c98a4ffc7d8bb5a9a6c93
Jonnathan23 <jsaquicela26@gmail.com>
2024-09-27 12:21:38 -0500

    Actualizado

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Metodos de ordenamiento");
+
+        int [] arreglo = {10,5,2,0,3};
+        int [] arregloArreglado = {};
+
+        MetodosOrdenamiento mO = new MetodosOrdenamiento();
+
+        arregloArreglado = mO.MethodSort(arreglo);
+        mO.printArray(arregloArreglado);
+
     }
 }
---
+++ src/MetodosOrdenamiento.java
@@ -0,0 +1,30 @@
+public class MetodosOrdenamiento {
+
+    public int[] MethodSort(int[] arreglo){
+        int lengthArray = arreglo.length;
+
+        for ( int i = 0; i < lengthArray; i++){
+            int indice = i;
+
+            for (int j = i + 1; j < lengthArray; j++){
+                if (arreglo[j] < arreglo[indice]){
+                    indice = j;
+                }
+            }
+            // Intercambio de indice
+            int aux = arreglo[indice];
+            arreglo[indice] = arreglo[i];
+            arreglo[i] = aux;
+        }
+        return arreglo;
+    }
+
+    public void printArray (int [] arreglo){
+        System.out.println();
+        for (int elemento : arreglo){
+            System.out.println(elemento + " ");
+        }
+    }
+
+
+}
